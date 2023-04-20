4b1cc6e9a15a3b75eb38624c2571fbce657251af
Surya Ven <suryaven@Suryas-MacBook-Pro-2.local>
2023-04-19 20:55:04 -0700

    start scheduler without reading args

--- starburst/drivers/main_driver.py
+++ starburst/drivers/main_driver.py
@@ -74,6 +74,59 @@ def main():
     finally:
         event_loop.close()
 
-
+starburst_scheduler = None
 if __name__ == '__main__':
     main()
+
+
+def custom_start(grpc_port=GRPC_PORT, sched_tick_time=SCHED_TICK_TIME, onprem_k8s_cluster_name=ONPREM_K8S_CLUSTER_NAME, cloud_k8s_cluster_name=CLOUD_K8S_CLUSTER_NAME, policy=POLICY_STR):
+    global startburst_scheduler
+    '''
+    # Parse command line arguments =================================================================
+    parser.add_argument('--grpc_port', type=int, default=GRPC_PORT,
+                        help='GRPC port to listen on')
+    parser.add_argument('--sched_tick_time', type=int, default=SCHED_TICK_TIME,
+                        help='Time between scheduler ticks')
+    parser.add_argument('--onprem_k8s_cluster_name', type=str, default=ONPREM_K8S_CLUSTER_NAME,
+                        help='Name of on-prem K8s cluster')
+    parser.add_argument('--cloud_k8s_cluster_name', type=str, default=CLOUD_K8S_CLUSTER_NAME,
+                        help='Name of cloud K8s cluster')
+    parser.add_argument('--policy', type=str, default=POLICY_STR,
+                        help='Policy to use. Currently supported - fifo_onprem_only')
+    args = parser.parse_args()
+    '''
+
+    # Create event queue, logger and sources ==================================================
+    event_queue = asyncio.Queue()
+    event_logger = SimpleEventLogger()
+    event_loop = asyncio.get_event_loop()
+    # Create event sources
+    sched_tick_event_source = SchedTickEventSource(event_queue, sched_tick_time)
+    grpc_job_submit_event_source = JobSubmissionEventSource(event_queue, grpc_port)
+    event_sources = [sched_tick_event_source, grpc_job_submit_event_source]   # Add more, such as GRPC event source here
+
+    # Pass learner bank and time series model to the scheduler =====================================
+    starburst = StarburstScheduler(event_queue,
+                                    event_logger,
+                                    onprem_cluster_name=onprem_k8s_cluster_name,
+                                    cloud_cluster_name=cloud_k8s_cluster_name,
+                                    queue_policy_str=policy)
+    
+    starburst_scheduler = starburst
+    for s in event_sources:
+        event_loop.create_task(s.event_generator())
+    try:
+        event_loop.run_until_complete(starburst.scheduler_loop())
+    finally:
+        event_loop.close()
+
+    #return starburst, event_sources, event_loop
+
+def start_events(starburst, event_sources, event_loop):
+    # Create event sources =========================================================================
+    for s in event_sources:
+        event_loop.create_task(s.event_generator())
+    try:
+        event_loop.run_until_complete(starburst.scheduler_loop())
+    finally:
+        event_loop.close()
