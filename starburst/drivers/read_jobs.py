83fd9bc768058de39b99926e322bac9a06ef449a
Surya Ven <suryaven@Suryas-MacBook-Pro-2.local>
2023-04-21 19:20:15 -0700

    draft grpc feature to retrieve job queue

--- starburst/drivers/read_jobs.py
+++ starburst/drivers/read_jobs.py
@@ -1,1 +1,14 @@
+import grpc
+
 # TODO: Read job queue information 
+
+DEFAULT_IP = 'localhost'
+DEFAULT_PORT = 10000
+
+def read_job_queue():
+	# TODO: Use RPC to retrieve job queue information from starburst scheduler 
+	# TODO: Follow implementation of submit_job.py and job_submit_event_source.py
+	return 
+
+def run_client(ip, port): 
+	return 
