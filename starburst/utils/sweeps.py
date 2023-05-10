910cc262caf2bf7d9d27e566e064f01c9c58df62
suryavengadesan <sury@berkeley.edu>
2023-05-10 00:54:57 +0000

    python file to track common sweep hyperparameters

---
+++ starburst/utils/sweeps.py
@@ -0,0 +1,48 @@
+import numpy as np
+
+def generate_interval(min=0, max=10, intervals=10):
+	return np.linspace(min, max, num=intervals+1).tolist()
+
+SWEEPS = {
+    "3": {
+        "fixed_values" : {
+            "batch_time": 180,
+            "mean_duration": 15,
+            "waiting_policy": "fifo_wait",
+            "cpu_sizes":[1, 2, 4],
+            "uniform_submission": True,
+            "uniform_arrival": 4
+        },
+        "varying_values" :{
+            "cpu_dist": [[0.6, 0.25, 0.15]],
+            "wait_time": [3],
+            "arrival_rate": [0.25, 0.5, 0.75, 1.0, 1.25, 1.5, 1.75, 2.0, 2.5, 3]
+        }
+    },
+    "2": {
+        "fixed_values" : {
+            "batch_time": 180,
+            "mean_duration": 15,
+            "waiting_policy": "fifo_wait",
+            "cpu_sizes":[1, 2, 4],
+        },
+        "varying_values" :{
+            "cpu_dist": [[0.6, 0.25, 0.15]],
+            "wait_time": [3],
+            "arrival_rate": [0.25, 0.5, 0.75, 1.0, 1.25, 1.5, 1.75, 2.0, 2.5, 3]
+        }
+    },
+    "1": {
+        "fixed_values" : {
+            "batch_time": 120,
+            "mean_duration": 10,
+            "waiting_policy": "fifo_wait",
+            "cpu_sizes":[1, 2, 4],
+        },
+        "varying_values" :{
+            "cpu_dist": [[0.2, 0.4, 0.4], [0, 0.5, 0.5], [0, 0, 1]],	
+            "wait_time": [0.1, 2.5, 5, 10],
+            "arrival_rate": [1/i for i in generate_interval(0.1, 3, 4)],
+        }
+    }
+}
