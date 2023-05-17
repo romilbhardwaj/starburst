735a841c51875d96a247231c4942076cfa296634
suryavengadesan <sury@berkeley.edu>
2023-05-17 05:44:55 +0000

    rewriting event loop

--- starburst/scheduler/starburst_scheduler.py
+++ starburst/scheduler/starburst_scheduler.py
@@ -7,6 +7,7 @@ import logging
 import os
 import queue
 import time
+import json 
 
 from starburst.policies import queue_policies
 from starburst.cluster_managers.kubernetes_manager import KubernetesManager
@@ -25,7 +26,9 @@ class StarburstScheduler:
                  cloud_cluster_name: str,
                  queue_policy_str: str = "fifo_onprem_only",
                  wait_time: int = 0,
-                 job_data: dict = {}):
+                 job_data: dict = {},
+                 timestamp: int = 0,
+                 run: int = 0):
         """
         Main Starburst scheduler class. Responsible for processing events in the provided event queue.
         :param event_queue: Main event queue
@@ -39,6 +42,12 @@ class StarburstScheduler:
         self.onprem_cluster_name = onprem_cluster_name
         self.cloud_cluster_name = cloud_cluster_name
         self.job_queue = []
+        self.prev_sched_tick_call = None
+        self.prev_tick = 0
+        self.prev_event = None 
+        self.timestamp = timestamp
+        self.ticks = []
+        self.run = run
 
         # Create the cluster managers
         self.onprem_cluster_manager = KubernetesManager(self.onprem_cluster_name)
@@ -63,10 +72,10 @@ class StarburstScheduler:
         :return:
         '''
         # TODO: Timeout event for job 
-        if event.event_type == EventTypes.SCHED_TICK:
-            assert isinstance(event, SchedTick)
-            self.processor_sched_tick_event(event)
-        elif event.event_type == EventTypes.JOB_ADD:
+        # if event.event_type == EventTypes.SCHED_TICK:
+        #     assert isinstance(event, SchedTick)
+        #     self.processor_sched_tick_event(event)
+        if event.event_type == EventTypes.JOB_ADD:
             assert isinstance(event, JobAddEvent)
             self.processor_job_add_event(event)
         else:
@@ -86,10 +95,45 @@ class StarburstScheduler:
         """ Process an add job event. This is where you probably want to add job to your queue"""
         self.job_queue.append(event.job)
 
+    '''
+    async def log_ticks(self): 
+        """Logs tick duration to reduce simulation and real world gap"""
+
+        tick_dir = "../logs/archive/" + str(self.timestamp) + "/ticks/"
+        if not os.path.exists(tick_dir):
+            os.mkdir(tick_dir)
+
+        while True: 
+            time.sleep(1)
+            tick_path = tick_dir + str(self.run) + ".json"
+            with open(tick_path, "w") as f:
+                json.dump(self.ticks, f)
+    '''
+
     async def scheduler_loop(self, queue, conn):
         """Main loop"""
         start_time = time.perf_counter()
         counter = 0 
+
+        #TODO: Ensure any two consequtive calls to SchedEventTick are limited by 
+        while True: 
+            _start_time = time.perf_counter()
+            self.queue_policy.process_queue(self.job_queue)
+            _end_time = time.perf_counter()
+            #logger.debug("LOOP TIME (()) " + str(_end_time))
+            #logger.debug("LOOP TIME (()) " + str(_end_time))
+            #logger.debug("LOOP TIME (()) " + str(_end_time - _start_time))
+            
+            scheduler_tick_interval = 0.5
+            if _end_time - _start_time > 0.5:
+                logger.debug("LOOP TIME (()) " + str(_end_time - _start_time))
+                continue
+            else:
+                time.sleep(0.5 - (_end_time - _start_time))
+            logger.debug("LOOP TIME (()) " + str(_end_time - _start_time))
+                
+            
+        '''
         while True:
             curr_time = time.perf_counter()
             logger.debug("Count(" + str(counter) + ") Loop start: ~~~ " + str(curr_time-start_time))
@@ -100,6 +144,9 @@ class StarburstScheduler:
             # Fetch event
             event = await self.event_queue.get()
 
+            # TODO: Spill out event queue until only one sched tick is left + any number of job add events -- don't congest the event queue
+            # TODO: Run process queue 
+
             curr_time = time.perf_counter()
             logger.debug("Fetched event: ~~~ " + str(curr_time-start_time))
 
@@ -108,10 +155,50 @@ class StarburstScheduler:
 
             curr_time = time.perf_counter()
             logger.debug("Logged event: ~~~ " + str(curr_time-start_time))
+            _start_time = time.perf_counter()
+            #if event.type == SchedTick: 
 
-            # Parse and handle event
-            self.process_event(event)
+            
+            #if event.event_type == EventTypes.SCHED_TICK:
+                #assert isinstance(event, SchedTick)
+                #if not self.prev_sched_tick_call:
+                    #self.prev_sched_tick_call = time.perf_counter()
+                    #self.process_event(event)
+                #else: 
+                    #curr_time = time.perf_counter()
+                    #while curr_time - self.prev_sched_tick_call < 0.5: 
+                        #curr_time = time.perf_counter()
+                        #time.sleep(0.0001)
+                    #self.prev_sched_tick_call = time.perf_counter()
+                    #self.process_event(event)
+            #else:
+                # Parse and handle event
+                #self.process_event(event)
+            
+            #self.process_event(event)
+            self.queue_policy.process_queue(self.job_queue)
 
             curr_time = time.perf_counter()
-
             logger.debug("End event processing and loop: ~~~ " + str(curr_time-start_time))
+
+            curr_time = time.perf_counter()
+            diff = curr_time - self.prev_tick
+            logger.debug("TICK TIME (()) " + str(diff) + " EVENT " + str(self.prev_event))
+            self.ticks.append((diff, self.prev_event))
+
+
+
+            self.prev_tick = curr_time
+            self.prev_event = event
+
+
+            _end_time = time.perf_counter()
+
+
+            logger.debug("LOOP TIME (()) " + str(_end_time))
+            scheduler_tick_interval = 0.5
+            if _end_time - _start_time > 0.5:
+                continue
+            else:
+                time.sleep(0.5 - (_end_time - _start_time)) 
+        '''
